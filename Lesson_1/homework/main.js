7e9625f05862c56e85d0a7a9e28c0384924e8b79
a_fluff <akryshneva8@gmail.com>
2021-07-20 11:59:37 +0300

    add homework_1

---
+++ Lesson_1/homework/main.js
@@ -0,0 +1,60 @@
+let result;
+result = "" + 1 + 0;
+console.log(result);
+console.log(typeof result);
+
+result = "" - 1 + 0;
+console.log(result);
+console.log(typeof result);
+
+result = true + false;
+console.log(result);
+console.log(typeof result);
+
+result = 6 / "3";
+console.log(result);
+console.log(typeof result);
+
+result = "2" * "3";
+console.log(result);
+console.log(typeof result);
+
+result = 4 + 5 + "px";
+console.log(result);
+console.log(typeof result);
+
+result = "$" + 4 + 5;
+console.log(result);
+console.log(typeof result);
+
+result = "4" - 2;
+console.log(result);
+console.log(typeof result);
+
+result = "4px" - 2;
+console.log(result);
+console.log(typeof result);
+
+result = 7 / 0;
+console.log(result);
+console.log(typeof result);
+
+result = " -9\n" + 5;
+console.log(result);
+console.log(typeof result);
+
+result = " -9\n" - 5;
+console.log(result);
+console.log(typeof result);
+
+result = null + 1;
+console.log(result);
+console.log(typeof result);
+
+result = undefined + 1;
+console.log(result);
+console.log(typeof result);
+
+result = null == "\n0\n";
+console.log(result);
+console.log(typeof result);
---
+++ Lesson_1/homework/main_2.js
@@ -0,0 +1,8 @@
+let x = 11;
+let y = 23;
+
+x += y;
+y = x - y;
+x -= y;
+
+console.log(x, y);
