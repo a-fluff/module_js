58acceb439792b6183d1536005d5c94675db015a
a_fluff <akryshneva8@gmail.com>
2021-07-20 11:09:37 +0300

    add Lesson_1

---
+++ Lesson_1/main.js
@@ -0,0 +1,65 @@
+// console.log('Hello World!');
+
+// let msg = 'It is good';
+// let num = 15;
+//
+// console.log(typeof msg);
+// console.log(typeof num);
+
+// let a = 10;
+// let b = 7;
+// let c = a + b;
+// let string = '12345';
+
+// console.log(-c);
+// console.log(typeof +string);
+
+// let remains = a % b;
+// console.log(remains);
+
+// let stepen = a ** b;
+// console.log(stepen);
+
+// a++;
+// b--;
+// console.log(a, b);
+
+// // a = a * 3;
+// a *= 3;
+// console.log(a);
+
+// let msg = 'Привет';
+// let name = ', Alex';
+// let msg2 = ', как дела?';
+// let result = msg + name + msg2 + ' Samsung';
+// let specialText = `
+// lorem
+//   inpsum
+// `;
+// console.log(result);
+// console.log(specialText);
+
+// console.log(50 > 30);
+// console.log(50 < 30);
+// console.log(50 >= 49);
+// console.log(50 == 50);
+// console.log(50 != 50);
+
+// console.log(50 == '50');
+// console.log(50 === '50');
+
+let flag = true;
+let num = '123';
+let str = 'one,\n two';
+console.log(str);
+let str = 'one,\' two';
+console.log(str);
+console.log(Number(flag));
+console.log(Number(num));
+console.log(Number(str));
+
+console.log(String(flag));
+console.log(String(num));
+
+console.log(Boolean(num));
+console.log(Boolean(str));
